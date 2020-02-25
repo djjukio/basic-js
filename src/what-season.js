2011ab8317f80acc35d70443dced5c508398817a
Andrey Alfirovich <djjukio@gmail.com>
2020-02-25 19:50:53 +0300

    feat: add solutions to count-cats, what-season, hanoi-tower

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,10 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date)) throw new Error;
+  for (item in date) throw new Error;
+  let month = date.getMonth();
+  if(month <= 1 || month === 11) return 'winter';
+  if(month <= 4) return 'spring';
+  if(month <= 7) return 'summer';
+  if(month <= 10) return 'autumn';
+  };
